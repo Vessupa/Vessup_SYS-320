81a8f54990a8867ab2e4b311e241023023907c4f
Vessupa <asani.vessup@mymail.champlain.edu>
2022-01-27 16:09:18 -0800

    "All Set"

---
+++ Week01/Homework/syslogCheck_Klogind.py
@@ -0,0 +1,26 @@
+import re
+
+def _syslog(filename,listOfKeywords):
+
+    with open(filename) as f:
+
+        contents = f.readlines()
+
+    # Lists to store the results
+    results = []
+    
+
+    for line in contents:
+
+        for eachKeyword in listOfKeywords:
+
+
+            x = re.findall(r''+eachKeyword+'', line)
+
+            for found in x:
+
+                #Append results to results list
+
+                    results.append(found)
+    
+    return results
